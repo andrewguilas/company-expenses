95881813f75a6558b2c310615d007694cce6d874
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2024-12-17 18:36:08 -0500

    Added filter by expenses/revenues

--- gui/expenses_scene.py
+++ gui/expenses_scene.py
@@ -8,9 +8,10 @@ def strip_string(string):
 class ExpensesScene:
     def __init__(self):
         self.database = Database()
+        self.entries = []
 
     def build(self):
-        self.frame = tk.Frame(self.root, name="expenses_scene")
+        self.frame = tk.Frame(self.root, name="entries_scene")
 
         self.search_label = tk.Label(self.frame, text="Search by column:")
         self.search_label.grid(row=0, column=0, padx=10, pady=5)
@@ -26,10 +27,17 @@ class ExpensesScene:
         self.tree.heading("location", text="Location")
         self.tree.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
+        self.expenses_button = tk.Button(self.frame, text="Show Expenses", command=self.filter_by_expenses)
+        self.expenses_button.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
+
+        self.revenues_button = tk.Button(self.frame, text="Show Revenues", command=self.filter_by_revenues)
+        self.revenues_button.grid(row=2, column=1, padx=10, pady=5, sticky="ew")
+
         # Configure the row and column to expand with window resizing
         self.frame.grid_rowconfigure(1, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
 
+        self.filter_by_expenses()
         self.update_tree()
 
     def show(self, app):
@@ -50,10 +58,17 @@ class ExpensesScene:
         for row in self.tree.get_children():
             self.tree.delete(row)
 
-        expenses = self.database.get_expenses()
-        for expense in expenses:
-            if self.meets_search_query(expense, search_query):
-                self.tree.insert("", "end", values=(expense[1], expense[3], expense[4], expense[5], expense[6]))
+        for entry in self.entries:
+            if self.meets_search_query(entry, search_query):
+                self.tree.insert("", "end", values=(entry[1], entry[3], entry[4], entry[5], entry[6]))
 
-    def meets_search_query(self, expense, search_query):
-        return strip_string(search_query) in strip_string(str(expense))
+    def meets_search_query(self, entry, search_query):
+        return strip_string(search_query) in strip_string(str(entry))
+
+    def filter_by_expenses(self):
+        self.entries = self.database.get_expenses()
+        self.update_tree()
+
+    def filter_by_revenues(self):
+        self.entries = self.database.get_revenues()
+        self.update_tree()
