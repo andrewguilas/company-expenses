afc1c647c46960313e3dbcffe272a3a682c1f44b
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2024-12-18 20:07:04 -0500

    Passed test get entries

--- tests/test_database.py
+++ tests/test_database.py
@@ -21,7 +21,6 @@ class TestDatabase(unittest.TestCase):
             location="new york"
         )
         self.database.add_entry(entry)
-        
         entries = self.database.get_entries()
         self.assertEqual(len(entries), 1)
         self.assertEqual(entries[0].description, "strawberry cheesecake")
@@ -38,3 +37,26 @@ class TestDatabase(unittest.TestCase):
         self.database.add_entry(entry)
         with self.assertRaises(DuplicateEntryError):
             self.database.add_entry(entry)
+
+    def test_get_entries(self):
+        entry1 = Entry(
+            date=datetime(2023, 12, 18).date(),
+            type=EntryType.EXPENSE,
+            category="food",
+            description="strawberry cheesecake",
+            amount=15.75,
+            location="new york"
+        )
+        entry2 = Entry(
+            date=datetime(2023, 12, 19).date(),
+            type=EntryType.EXPENSE,
+            category="food",
+            description="chocolate strawberry cheesecake",
+            amount=16.75,
+            location="new york"
+        )
+        self.database.add_entry(entry1)
+        self.database.add_entry(entry2)
+        entries = self.database.get_entries()
+        self.assertEqual(len(entries), 2)
+        self.assertEqual(entries[1].type, EntryType.REVENUE)
