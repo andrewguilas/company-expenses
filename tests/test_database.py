4affce277a8760f1e31e5a0255b68335baf9657c
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2024-12-18 20:06:04 -0500

    Passed test duplicate entry

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import unittest
 
-from managers.database import Database
+from managers.database import Database, DuplicateEntryError
 from managers.entry import Entry, EntryType
 
 class TestDatabase(unittest.TestCase):
@@ -21,6 +21,20 @@ class TestDatabase(unittest.TestCase):
             location="new york"
         )
         self.database.add_entry(entry)
+        
         entries = self.database.get_entries()
         self.assertEqual(len(entries), 1)
         self.assertEqual(entries[0].description, "strawberry cheesecake")
+
+    def test_duplicate_entry(self):
+        entry = Entry(
+            date=datetime(2023, 12, 18).date(),
+            type=EntryType.EXPENSE,
+            category="food",
+            description="strawberry cheesecake",
+            amount=15.75,
+            location="new york"
+        )
+        self.database.add_entry(entry)
+        with self.assertRaises(DuplicateEntryError):
+            self.database.add_entry(entry)
